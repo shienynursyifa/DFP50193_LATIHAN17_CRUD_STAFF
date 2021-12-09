2000f26a57765cd9c8fbd62f3a539d622b8aa088
shienynursyifa <shienynursyifa@gmail.com>
2021-12-09 08:23:06 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -4,5 +4,4 @@ $user = 'root';
 $pswd = ''; # sepasang single quotes
 $dbname = 'contoh_login';
 
-$conn = new mysqli($host, $user, $pswd, $dbname);
-session_start();
+$conn = new mysqli($host, $user, $pswd, $dbname); session_start();
